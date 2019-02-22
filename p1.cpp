caca4c01342bbb4c05b7a244ac15057c05f58f52
Sotalora97 <sotalora_21@hotmail>
2019-02-22 10:35:00 -0500

    Que relatan las ratas

---
+++ p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+ cout<<"ingrese los datos del histograma"<<endl;
+ cin>> n;
+ cout<<"desea ingresar mas datos?"<<endl;
+
+
+
+
+
+
+
+
+}
